cf4813611fbf0159bca22e84e1e3173549d86272
Stable Release <stable@drift.app>
2026-05-14 21:15:30 +0530

    feat: added missing withIosModifications plugin file

---
+++ plugins/withIosModifications.js
@@ -0,0 +1,19 @@
+const { withProjectBuildProperties } = require('@expo/config-plugins');
+
+/**
+ * Custom Plugin to fix Xcode 16 / Firebase Precompilation Failures
+ */
+const withIosModifications = (config) => {
+  return withProjectBuildProperties(config, {
+    ios: {
+      deploymentTarget: '15.1',
+      useFrameworks: 'static',
+      extraPods: [
+        { name: 'FirebaseCore', modular_headers: true },
+        { name: 'FirebaseCrashlytics', modular_headers: true }
+      ]
+    }
+  });
+};
+
+module.exports = withIosModifications;
